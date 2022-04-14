4d36343772b25e587f8217ae2cb814c5bd6ca2ca
Omar <olr7742@nyu.edu>
2022-04-14 18:29:22 +0400

    updated_sign_in

--- Sign_In_Page.py
+++ Sign_In_Page.py
@@ -2,7 +2,7 @@ import pygame
 import InputBox
 
 pygame.init()
-screen = pygame.display.set_mode(1600, 900)
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 screen_width, screen_height = pygame.display.get_surface().get_size()
 
 background = pygame.image.load("Background_Home_Page.png")
@@ -14,13 +14,15 @@ start_button_width = 592
 start_button_height = 76
 start_button_x = 503
 start_button_y = 679
+start_button_color = (255, 195, 0)
+start_button_text_color = (0, 0, 0)
 font = pygame.font.Font('Futura-Bold.otf', 36)
-text = font.render("START", True, (0, 0, 0))
-text_rect = pygame.Rect(screen_width/2 - text.get_width()/2,
-                        start_button_y+text.get_height()/2, 0, 0)
-screen.blit(text, text_rect)
-rect = pygame.Rect(start_button_x, start_button_y,
-                   start_button_width, start_button_height)
+start_text = font.render("START", True, start_button_text_color)
+text_rect = pygame.Rect(screen_width/2 - start_text.get_width()/2,
+                        start_button_y+start_text.get_height()/2, 0, 0)
+screen.blit(start_text, text_rect)
+start_rect = pygame.Rect(start_button_x, start_button_y,
+                         start_button_width, start_button_height)
 
 # Username Field
 username_field_width = 592
@@ -40,12 +42,32 @@ password_field = InputBox.InputBox(
 
 def setup_screen():
     screen.blit(background, (0, 0))
-    pygame.draw.rect(screen, (255, 195, 0), rect,
+    pygame.draw.rect(screen, start_button_color, start_rect,
                      0, int(start_button_height/2))
-    pygame.draw.rect(screen, (0, 0, 0), rect, 2, int(start_button_height/2))
+    pygame.draw.rect(screen, (0, 0, 0), start_rect,
+                     2, int(start_button_height/2))
     username_field.draw(screen)
     password_field.draw(screen)
-    screen.blit(text, text_rect)
+    screen.blit(start_text, text_rect)
+    # placeholders
+    if password_field.get_text() == "":
+        font = pygame.font.Font('Futura-Medium.otf', 30)
+        password_placeholder_text = font.render(
+            "Password", True, (150, 150, 150))
+        password_placeholder_text_rect = pygame.Rect(screen_width/2 - password_placeholder_text.get_width(
+        )/2, password_field_y + password_placeholder_text.get_height()/2, 0, 0)
+        screen.blit(password_placeholder_text, password_placeholder_text_rect)
+    if username_field.get_text() == "":
+        font = pygame.font.Font('Futura-Medium.otf', 30)
+        username_placeholder_text = font.render(
+            "Username", True, (150, 150, 150))
+        username_placeholder_text_rect = pygame.Rect(screen_width/2 - username_placeholder_text.get_width(
+        )/2, username_field_y + username_placeholder_text.get_height()/2, 0, 0)
+        screen.blit(username_placeholder_text, username_placeholder_text_rect)
+
+
+def clicked_start():
+    print("clicked_start")
 
 
 print(screen_height, screen_width)
@@ -58,5 +80,17 @@ while run:
         if e.type == pygame.KEYDOWN:
             if e.key == pygame.K_q:
                 run = False
+        if e.type == pygame.MOUSEBUTTONDOWN and start_rect.collidepoint(e.pos):
+            clicked_start()
+        if start_rect.collidepoint(pygame.mouse.get_pos()):
+            start_button_color = (0, 0, 0)
+            start_button_text_color = (255, 195, 0)
+            start_text = font.render("START", True, start_button_text_color)
+
+        else:
+            start_button_color = (255, 195, 0)
+            start_button_text_color = (0, 0, 0)
+            start_text = font.render("START", True, start_button_text_color)
+
     pygame.display.flip()
 pygame.quit()
